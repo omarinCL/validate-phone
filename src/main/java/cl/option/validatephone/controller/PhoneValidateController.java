b69b3157aa0217a5e87d0b1f1f5dbfbfda0a6c7e
Orlando Marin <orlando.marin@outlook.cl>
2020-02-12 23:43:32 -0300

    CORS

--- src/main/java/cl/option/validatephone/controller/PhoneValidateController.java
+++ src/main/java/cl/option/validatephone/controller/PhoneValidateController.java
@@ -5,8 +5,10 @@ import javax.validation.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,7 @@ import cl.option.validatephone.service.PhoneValidation;
 @RestController
 @RequestMapping("/api/v1/phone")
 @Validated
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET}) 
 public class PhoneValidateController {
     @Autowired
     private PhoneValidation service;
